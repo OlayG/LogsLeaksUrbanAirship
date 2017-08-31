d28cda29e7d6d76d24de01c3ec838c13da7568eb
olayg <ogureje@yahoo.com>
2017-08-30 20:54:18 -0400

    Setup Barcode Scanner

---
+++ app/src/main/java/com/example/admin/logsleaksurbanairship/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.admin.logsleaksurbanairship;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnScanCode)
+    public void onViewClicked() {
+        scannerView = new ZXingScannerView(this);
+        scannerView.setResultHandler(new ZXingScannerResultHandler());
+
+        setContentView(scannerView);
+        scannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    class ZXingScannerResultHandler implements ZXingScannerView.ResultHandler{
+
+        @Override
+        public void handleResult(Result result) {
+            String resultCode = result.getText();
+            Toast.makeText(MainActivity.this, resultCode, Toast.LENGTH_SHORT).show();
+
+            setContentView(R.layout.activity_main);
+            scannerView.stopCamera();
+        }
+    }
+}
